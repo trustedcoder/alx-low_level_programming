54d29a3ffb8e432d5e6b37babd3ef74ea62f81c3
trustedcoder <trustedcoder11@gmail.com>
2022-12-09 17:46:43 -0800

    f

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'type of integer in a random numberd'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		if (i < 10)
+		{
+			putchar('0' + i);
+		}
+		else
+		{
+			putchar('a' + i - 10);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
