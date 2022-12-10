7ef89e0698db5586437148d463b8a11088b61e96
trustedcoder <trustedcoder11@gmail.com>
2022-12-09 17:52:05 -0800

    f

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'type of integer in a random numberd'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar(',');
+			putchar(' ');
+			putchar('0' + j);
+			putchar('\n');
+		}
+	}
+	return (0);
+}
