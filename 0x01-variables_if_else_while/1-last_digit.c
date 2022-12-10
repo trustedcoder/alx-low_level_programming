43fd20e6e0e3d148ef8b4839b9483a68d8596db8
trustedcoder <trustedcoder11@gmail.com>
2022-12-10 01:23:11 +0100

    checks

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,12 +14,12 @@ int main(void)
 {
     int n;
     int last_digit;
-    
+
     srand(time(0));
     n = rand() - RAND_MAX / 2;
-    
+
     last_digit = n % 10;
-    
+
 
     if (last_digit > 5)
     {
