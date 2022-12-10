a50d4a5f1f066d7ce80b605cac23110b88f5628a
trustedcoder <trustedcoder11@gmail.com>
2022-12-09 17:27:56 -0800

    f

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'type of integer in a random numberd'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+		{
+			continue;
+		}
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
