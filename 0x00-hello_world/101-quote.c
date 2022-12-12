8370f04b742b5dc1b82d8d08f46a7acaed8f7c24
trustedcoder <trustedcoder11@gmail.com>
2022-12-12 13:22:13 -0800

    f

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'type of integer in a random numberd'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
