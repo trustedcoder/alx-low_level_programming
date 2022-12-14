afc33dc44ea75fdf9623aa586b01efacb7ce9e65
trustedcoder <trustedcoder11@gmail.com>
2022-12-13 20:25:44 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'type of integer in a random numberd'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", "putchar");
+	return (0);
+}
