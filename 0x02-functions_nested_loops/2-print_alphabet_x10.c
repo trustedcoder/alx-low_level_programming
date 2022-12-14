bfb656431cef4ab4a09087b73f1331e67eeba97e
trustedcoder <trustedcoder11@gmail.com>
2022-12-13 20:52:27 -0800

    r

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - Entry
+ * Description: 'type of integer in a random numberd'
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
